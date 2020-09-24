3773422235fc07d2457e5fa539a864cc97c1f155
Ankush Chourasia <ankushchourasia@hotmail.com>
2020-09-24 17:06:13 +0530

    Updates

---
+++ precache-manifest.7d0423d27be347d92920d87fc318c975.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93b75b0c57b9cd2391b10495e02c23e",
+    "url": "/React-Application/index.html"
+  },
+  {
+    "revision": "60cae3511a7ec5762729",
+    "url": "/React-Application/static/css/main.9d26782c.chunk.css"
+  },
+  {
+    "revision": "75291e63a9248fc78d39",
+    "url": "/React-Application/static/js/2.38f76c7b.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/React-Application/static/js/2.38f76c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60cae3511a7ec5762729",
+    "url": "/React-Application/static/js/main.370ce780.chunk.js"
+  },
+  {
+    "revision": "c8a6a3862f18ffc9075c",
+    "url": "/React-Application/static/js/runtime-main.002641a6.js"
+  },
+  {
+    "revision": "ea3e66a1fcb8d9aab29dbc1429a83bdd",
+    "url": "/React-Application/static/media/noLocation.ea3e66a1.png"
+  }
+]);
